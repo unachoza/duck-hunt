91a1614eb6ed09ad346e549f8e4c2dbae2a1e211
arianna choza <unachoza@gmail.com>
2020-06-22 11:39:01 -0400

    add duck button works, dog visible, always, needs work

--- js/duckhunt.js
+++ js/duckhunt.js
@@ -1,86 +1,81 @@
-// const body = document.body;
+const body = document.body;
 
-//   const dog = document.getElementsByClassName('dog')
-//   console.log(dog)
+const dog = document.getElementsByClassName('dog');
+console.log(dog);
 
 window.onload = function () {
   const body = document.body;
 
-  var dog = document.getElementsByClassName('dog')
-  console.log(dog)
- 
+  var dog = document.getElementsByClassName('dog');
+  console.log(dog);
 
+  // 1. Create a <div> with the class "duck" and add it to the body.
+  const duckdiv = document.createElement('div');
+  duckdiv.classList.add('duck');
+  body.appendChild(duckdiv);
 
-  // 1. Create a <div> with the class "duck" and add it to the body.  
-  const duckdiv = document.createElement("div")
-  duckdiv.classList.add('duck')
-  body.appendChild(duckdiv)
-
-  duckdiv.classList.add("flap")
+  duckdiv.classList.add('flap');
 
   setInterval(function () {
-    duckdiv.classList.toggle("flap")
-  }, 250)
+    duckdiv.classList.toggle('flap');
+  }, 250);
 
   function moveDuck() {
-    duckdiv.style.left = (Math.random() * window.innerWidth) + "px";
-    duckdiv.style.top = (Math.random() * window.innerHeight) + "px";
-
+    duckdiv.style.left = Math.random() * window.innerWidth + 'px';
+    duckdiv.style.top = Math.random() * window.innerHeight + 'px';
   }
-  setInterval(moveDuck, 1000)
+  setInterval(moveDuck, 1000);
 
   function createDuck() {
     const body = document.body;
-    const duckdiv = document.createElement("div")
-    duckdiv.classList.add('duck')
-    body.appendChild(duckdiv)
-    duckdiv.classList.add("flap")
+    const duckdiv = document.createElement('div');
+    duckdiv.classList.add('duck');
+    body.appendChild(duckdiv);
+    duckdiv.classList.add('flap');
     setInterval(function () {
-      duckdiv.classList.toggle("flap")
-    }, 250)
+      duckdiv.classList.toggle('flap');
+    }, 250);
 
     function moveDuck() {
-      duckdiv.style.left = (Math.random() * window.innerWidth) + "px";
-      duckdiv.style.top = (Math.random() * window.innerHeight) + "px";
-
+      duckdiv.style.left = Math.random() * window.innerWidth + 'px';
+      duckdiv.style.top = Math.random() * window.innerHeight + 'px';
     }
-    setInterval(moveDuck, 1000)
+    setInterval(moveDuck, 1000);
 
-    return duckdiv
+    return duckdiv;
   }
-  let defaultNumDucks = 2
+  let defaultNumDucks = 2;
 
   //add duck button
-  const button = document.querySelector(".addDuck");
+  const button = document.querySelector('.addDuck');
   //console.log(button)
-  button.addEventListener("click", function () {
-    console.log('add a duck')
-    defaultNumDucks += 1
-    console.log(defaultNumDucks)
-    return defaultNumDucks
-  })
+  button.addEventListener('click', function () {
+    console.log('add a duck');
+    defaultNumDucks += 1;
+    console.log(defaultNumDucks);
+    return defaultNumDucks;
+  });
 
   for (let i = 0; i < defaultNumDucks; i++) {
-    createDuck()
+    createDuck();
 
     //select all ducks
     //loop through them
   }
-  const ducks = document.querySelectorAll('.duck')
+  const ducks = document.querySelectorAll('.duck');
 
-  console.log(ducks)
+  console.log(ducks);
 
   for (let i = 0; i < ducks.length; i++) {
     ducks[i].addEventListener('click', function (e) {
       // console.log(e)
       // console.log('i clicked')
-      ducks[i].classList.add('shot')
+      ducks[i].classList.add('shot');
       setTimeout(() => {
-        ducks[i].remove()
-
+        ducks[i].remove();
       }, 500);
       checkForWinner();
-    })
+    });
     // function removeDuck(event){
     //   event.target.remove()
     // }
@@ -91,32 +86,24 @@ window.onload = function () {
     // duckdiv.classList.add('shot')
   }
 
-
-
-
-
-
-  let numDucks = document.getElementsByClassName('duck').length
-  console.log(numDucks)
-
-
-
+  let numDucks = document.getElementsByClassName('duck').length;
+  console.log(numDucks);
 
   function checkForWinner() {
-    console.log('grab the ducks')
-    let numDucks = document.getElementsByClassName('duck').length - 1
-    console.log(numDucks)
-    let dog = document.getElementsByClassName('dog')
+    console.log('grab the ducks');
+    let numDucks = document.getElementsByClassName('duck').length - 1;
+    console.log(numDucks);
+    let dog = document.getElementsByClassName('dog');
     if (numDucks === 0) {
-      let dog = document.getElementsByClassName('dog')
-      dog.style.visibility = "visible";
-      alert('YOU WIN')
+      console.log('winnnnnner ya hoo!!!');
+      dog.style.display = 'visble';
+      dog.style.visibility = 'visible';
+      alert('YOU WIN');
       //dog.style.visibility = "visible";
-      
+
       //body.appendChild(dog)
       //console.log
     }
-
   }
   //checkForWinner()
 
