a0741b90affdf8a3d0381e66605be94f270d10b9
arianna choza <unachoza@gmail.com>
2020-06-23 12:47:57 -0400

    play again and start game are the same button with different functions, one starts the game the other ends the game, and everything works, need refactor

--- js/cleanerscript.js
+++ js/cleanerscript.js
@@ -2,16 +2,33 @@ let defaultSpeed = 900;
 let defaultNumDucks = 2; //number of ducks
 
 window.onload = function () {
-  const startAudio = new Audio('https://res.cloudinary.com/dh41vh9dx/video/upload/v1592923996/start-round.mp3');
-  startAudio.play();
-
   const body = document.body;
-  // document.getElementById('dogWithDeadDucks').classList.add('hide');
-  document.querySelector('.popup').classList.add('hide');
+
+  // const startAudio = new Audio('https://res.cloudinary.com/dh41vh9dx/video/upload/v1592923996/start-round.mp3');
+  // startAudio.play();
+  // let audio = document.getElementById('start-audio');
+  // audio.play();
+  // setTimeout(() => audio.play(), 1000);
+  document.querySelector('#start-popup').classList.add('hide');
+  document.querySelector('#end-popup').classList.add('hide');
+  const showPopup = (element) => {
+    element.classList.remove('hide');
+    element.classList.add('popup-open');
+  };
+  const hidePopup = (element) => {
+    element.classList.add('hide');
+    element.classList.remove('popup-open');
+  };
+
+  showPopup(document.querySelector('#start-popup'));
+
+  document.querySelector('#start-button').addEventListener('click', () => {
+    console.log('this is the start button and I can spell that');
+    makeTheDucksAndStartTheGame();
+  });
 
   const createDuck = () => {
     //add duck
-    startAudio.play();
     const duck = document.createElement('div');
     duck.classList.add('duck');
     body.appendChild(duck);
@@ -35,31 +52,34 @@ window.onload = function () {
     }
     return duck;
   };
+  const makeTheDucksAndStartTheGame = () => {
+    console.log('starting after click');
+    hidePopup(document.querySelector('#start-popup'));
+    for (let i = 0; i < defaultNumDucks; i++) {
+      //loop creating multiple ducks
+      createDuck();
+    }
+  };
 
-  for (let i = 0; i < defaultNumDucks; i++) {
-    //loop creating multiple ducks
-    createDuck();
-  }
   // add duck addDuckButton
   document.querySelector('.addDuck').addEventListener('click', () => createDuck());
-  document.querySelector('#playAgain').addEventListener('click', () => location.reload());
+  document.querySelector('#playAgain-button').addEventListener('click', () => {
+    console.log('I did win and now I am plahying again');
+    hidePopup(document.querySelector('#end-popup'));
+    makeTheDucksAndStartTheGame();
+    document.getElementById('dogWithDeadDucks').classList.remove('deadDogUp');
+  });
 
   const showDog = () => {
-    // document.getElementById('dogWithDeadDucks').classList.remove('hide');
     document.getElementById('dogWithDeadDucks').classList.add('deadDogUp');
   };
-  const showPopup = () => {
-    console.log('timeout');
-    document.querySelector('.popup').classList.remove('hide');
-    document.querySelector('.popup').classList.add('popup-open');
-    // document.querySelector('.popup__content').classList.add('content-open');
-  };
+
   const checkForWinner = () => {
     // You won Alert (+ dog )
     let numDucks = document.getElementsByClassName('duck').length - 1;
     if (numDucks === 0) {
       showDog();
-      setTimeout(() => showPopup(), 2000);
+      setTimeout(() => showPopup(document.querySelector('#end-popup')), 2000);
     }
   };
 };
