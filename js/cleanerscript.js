76547c36aba6eb6c810def7c63f0999e6fbafba6
arianna choza <unachoza@gmail.com>
2020-06-22 22:04:44 -0400

    works

--- js/cleanerscript.js
+++ js/cleanerscript.js
@@ -1,13 +1,9 @@
-var dog = document.getElementsByClassName('dog');
-console.log(dog[0]);
-dog = dog[0];
-
 window.onload = function () {
   const body = document.body;
-  //add dog
-  console.log(dog);
+  let ducks = document.querySelectorAll('.duck');
+  document.getElementById('dog').classList.add('hide');
 
-  function createDuck() {
+  const createDuck = () => {
     //add duck
     const duck = document.createElement('div');
     duck.classList.add('duck', 'flap');
@@ -17,55 +13,46 @@ window.onload = function () {
       duck.classList.toggle('flap');
     }, 250);
     //make duck fly
-    function moveDuck() {
+    const moveDuck = () => {
       duck.style.left = Math.random() * window.innerWidth + 'px';
       duck.style.top = Math.random() * window.innerHeight + 'px';
-    }
+    };
     //flying speed
     setInterval(moveDuck, 900);
+    ducks = document.querySelectorAll('.duck');
 
+    for (let i = 0; i < ducks.length; i++) {
+      ducks[i].addEventListener('click', () => {
+        ducks[i].classList.add('shot');
+        setTimeout(() => {
+          ducks[i].remove();
+        }, 200); //speed of duck removal
+        checkForWinner(); //calling check for winner function
+      });
+    }
     return duck;
-  }
+  };
   //number of ducks
   let defaultNumDucks = 1;
+  // add duck addDuckButton
+  document.querySelector('.addDuck').addEventListener('click', () => createDuck());
 
-  // add duck button
-  const button = document.querySelector('.addDuck');
-  console.log(button);
-  button.addEventListener('click', function () {
-    //console.log('add a duck')
-    createDuck();
-  });
+  // addDuckButton.addEventListener('click', () => createDuck());
 
   //loop creating multiple ducks
   for (let i = 0; i < defaultNumDucks; i++) {
     createDuck();
   }
-  //collecting those ducks in an  "ducks" array
-  const ducks = document.querySelectorAll('.duck');
-  console.log(ducks);
-  // adding shot to click event to each duck by looping through ducks array
-  for (let i = 0; i < ducks.length; i++) {
-    ducks[i].addEventListener('click', function () {
-      ducks[i].classList.add('shot');
-      setTimeout(() => {
-        ducks[i].remove();
-      }, 200); //speed of duck removal
-      checkForWinner(dog); //calling check for winner function
-    });
-  }
-
-  // tracking number of ducks
-  let numDucks = document.getElementsByClassName('duck').length;
-  console.log(numDucks);
   // You won Alert (+ dog ?)
-  function checkForWinner(dog) {
-    console.log(dog);
-    console.log('grab the ducks');
+  const showDog = () => {
+    console.log('please show', dog);
+    document.getElementById('dog').classList.remove('hide');
+  };
+  const checkForWinner = () => {
     let numDucks = document.getElementsByClassName('duck').length - 1;
-    console.log(numDucks);
-
-    numDucks === 0 ? (dog.style.right = 450) : console.log('youwin');
-  }
+    console.log(numDucks, 'the number of');
+    if (numDucks === 0) {
+      showDog();
+    }
+  };
 };
-// };
--- js/objectOrientedDucking.js
+++ js/objectOrientedDucking.js
@@ -1,35 +1,56 @@
 ////// //what is in a duck////////
-class duck {
+class Duck {
   constructor() {
     // duck Image
     // function duck flapping
+
     this.defaultNumDucks = 1;
+    this.speak = () => {
+      console.log('quack');
+    };
     this.flap = () => {
       setInterval(() => {
         this.duck.classList.toggle('flap');
       }, 250);
     };
-    this.moveDuck = () => {
-      duck.style.left = Math.random() * window.innerWidth + 'px';
-      duck.style.top = Math.random() * window.innerHeight + 'px';
-    };
-    this.changeDuckSpeed = (speed) => {
-      setInterval(this.moveDuck, speed);
-    };
+
+    // this.moveDuck = (duck) => {
+    //   duck.style.left = Math.random() * window.innerWidth + 'px';
+    //   duck.style.top = Math.random() * window.innerHeight + 'px';
+    // };
+
+    // this.changeDuckSpeed = (speed) => {
+    //   setInterval(this.moveDuck, speed);
+    // };
+
     this.createDuck = () => {
+      let body = document.body;
+      console.log(body);
       const duck = document.createElement('div');
-      duck.classList.add('duck', 'flap');
-      body.appendChild(duck);
-      this.flap();
-      this.moveDuck();
+      console.log(duck);
+      duck.classList.add('duck');
+      document.body.appendChild(duck);
+      // this.flap(this.duck);
+      // this.moveDuck(duck);
       return duck;
     };
+    this.addDuckFeatures = () => {};
 
-    // function duck creating, keeping the ducks together, like an array or node collection or something
-    // function ducking moving x and y
-    // function duck moving faster than before
-    // function ducks being shot
+    // this.shootDuck = () => {
+    //   duck.addEventListener('click', () => {
+    //     duck.classList.add('shot');
+    //     setTimeout(() => {
+    //       ducks[i].remove();
+    //     }, 200); //speed of duck removal
+    //     checkForWinner(dog); //calling check for winner function
+    //   });
+    // };
   }
+
+  // function duck creating, keeping the ducks together, like an array or node collection or something
+  // function ducking moving x and y
+  // function duck moving faster than before
+  // function ducks being shot
 }
 
 class dog {
@@ -42,5 +63,12 @@ class dog {
 }
 
 // then there are buttons
-
-let duck = new duck();
+window.onload = function () {
+  let firstDuck = new Duck();
+  let secondDuck = new Duck();
+  let thirdDuck = new Duck();
+  let fourthDuck = new Duck();
+  firstDuck.createDuck();
+  firstDuck.flap();
+  firstDuck.speak();
+};
