beb9333b7cb8a713ba9e282dce3049fd1be9c266
arianna choza <unachoza@gmail.com>
2020-06-23 14:20:42 -0400

    added media queries works for everything

--- js/cleanerscript.js
+++ js/cleanerscript.js
@@ -1,48 +1,40 @@
 let defaultSpeed = 900;
-let defaultNumDucks = 2; //number of ducks
+let defaultNumDucks = 4;
+///////////////////////////////////REFACTOR THIS , changePopup(popup, changeState)
+const showPopup = (element) => {
+  element.classList.remove('hide');
+  element.classList.add('popup-open');
+};
+const hidePopup = (element) => {
+  element.classList.add('hide');
+  element.classList.remove('popup-open');
+};
+///////////////////////////////////REFACTOR THIS^^^^^
 
 window.onload = function () {
   const body = document.body;
-
-  // const startAudio = new Audio('https://res.cloudinary.com/dh41vh9dx/video/upload/v1592923996/start-round.mp3');
-  // startAudio.play();
-  // let audio = document.getElementById('start-audio');
-  // audio.play();
-  // setTimeout(() => audio.play(), 1000);
-  document.querySelector('#start-popup').classList.add('hide');
-  document.querySelector('#end-popup').classList.add('hide');
-  const showPopup = (element) => {
-    element.classList.remove('hide');
-    element.classList.add('popup-open');
-  };
-  const hidePopup = (element) => {
-    element.classList.add('hide');
-    element.classList.remove('popup-open');
-  };
+  document.querySelector('#start-popup').classList.add('hide'); //hiding popups
+  document.querySelector('#end-popup').classList.add('hide'); //hiding popups
 
   showPopup(document.querySelector('#start-popup'));
 
   document.querySelector('#start-button').addEventListener('click', () => {
-    console.log('this is the start button and I can spell that');
     makeTheDucksAndStartTheGame();
   });
 
   const createDuck = () => {
-    //add duck
     const duck = document.createElement('div');
     duck.classList.add('duck');
     body.appendChild(duck);
-
     const moveDuck = () => {
       //make duck fly
       duck.style.left = Math.random() * window.innerWidth + 'px';
       duck.style.top = Math.random() * window.innerHeight + 'px';
     };
-
     setInterval(() => duck.classList.toggle('flap'), 250); // flapping speed
     setInterval(moveDuck, defaultSpeed); //flying speed
-    let ducks = document.querySelectorAll('.duck');
 
+    let ducks = document.querySelectorAll('.duck'); //all ducks
     for (let i = 0; i < ducks.length; i++) {
       ducks[i].addEventListener('click', () => {
         ducks[i].classList.add('shot');
@@ -52,26 +44,21 @@ window.onload = function () {
     }
     return duck;
   };
-  const makeTheDucksAndStartTheGame = () => {
-    console.log('starting after click');
-    hidePopup(document.querySelector('#start-popup'));
 
+  const makeTheDucksAndStartTheGame = () => {
+    hidePopup(document.querySelector('#start-popup'));
     const startAudio = new Audio('https://res.cloudinary.com/dh41vh9dx/video/upload/v1592923996/start-round.mp3');
     startAudio.volume = 0.1;
     startAudio.play();
-
     setTimeout(() => {
       for (let i = 0; i < defaultNumDucks; i++) {
-        //loop creating multiple ducks
         createDuck();
       }
     }, 2000);
   };
 
-  // add duck addDuckButton
   document.querySelector('.addDuck').addEventListener('click', () => createDuck());
   document.querySelector('#playAgain-button').addEventListener('click', () => {
-    console.log('I did win and now I am plahying again');
     hidePopup(document.querySelector('#end-popup'));
     makeTheDucksAndStartTheGame();
     document.getElementById('dogWithDeadDucks').classList.remove('deadDogUp');
